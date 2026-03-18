63d94059d2b8ba5b5e3df0e7b30bb1cb782fe6ae
secretboylko <adityasinghlko000@gmail.com>
2026-03-18 15:13:25 +0530

    Update config.py
    Signed-off-by: secretboylko <adityasinghlko000@gmail.com>

--- config.py
+++ config.py
@@ -38,7 +38,7 @@ class Config:
 
         # 🍪 Cookies (FIXED – any URL works)
         self.COOKIES_URL = [
-            url for url in getenv("COOKIES_URL", "https://pastebin.com/raw/bKE6fy82").split(" ")
+            url for url in getenv("COOKIES_URL", "").split(" ")
             if url
         ]
 
